667bbd8a1640206eba3fa71653826b19dfe57706
Peter Halliday <pghalliday@gmail.com>
2012-11-02 01:18:40 +0100

    dynamic module loading

--- Require/src/OOORequire.c
+++ Require/src/OOORequire.c
@@ -2,32 +2,93 @@
 
 #define OOOClass OOORequire
 
-typedef OOOModule * (* GetModule)(void);
+typedef OOOModule * (* Init)(void);
+typedef void (* Uninit)(void);
+
+typedef struct _ModuleEntry ModuleEntry;
+struct _ModuleEntry
+{
+	char * szModuleName;
+	unsigned char * pModuleData;
+	OOOModule * pModule;
+	Uninit uninit;
+	ModuleEntry * pNext;
+};
 
 OOOPrivateData
 	OOOIRepository * iRepository;
+	ModuleEntry * pModules;
 OOOPrivateDataEnd
 
 OOODestructor
 {
+	ModuleEntry * pEntry = OOOF(pModules);
+	ModuleEntry * pLastEntry = NULL;
+	while (pEntry)
+	{
+		pLastEntry = pEntry;
+		pEntry = pEntry->pNext;
+
+		if (pLastEntry->uninit)
+		{
+			pLastEntry->uninit();
+		}
+		else
+		{
+			O_debug("WARNING: Code module has no uninit method: %s\n", pLastEntry->szModuleName);
+		}
+
+		if (O_dl_unlink_code_module(pLastEntry->pModuleData) != GOOD)
+		{
+			O_debug("WARNING: Failed to unlink code module: %s\n", pLastEntry->szModuleName);
+		}
+
+		O_free(pLastEntry->szModuleName);
+		O_free(pLastEntry);
+	}
 }
 OOODestructorEnd
 
 OOOMethod(void, get, char * szModuleName, OOOIRequirer * iRequirer)
 {
 	OOOModule * pModule = NULL;
-	unsigned char * pModuleData = OOOICall(OOOF(iRepository), get, szModuleName);
-	if (pModuleData)
+	ModuleEntry * pEntry = OOOF(pModules);
+	while (pEntry)
 	{
-		if (O_dl_link_code_module(pModuleData) == GOOD)
+		if (O_strcmp(pEntry->szModuleName, szModuleName) == 0)
 		{
-			GetModule getModule = O_dl_find_function(0);
-			if (getModule)
+			pModule = pEntry->pModule;
+			break;
+		}
+		pEntry = pEntry->pNext;
+	}
+
+	if (!pModule)
+	{
+		unsigned char * pModuleData = OOOICall(OOOF(iRepository), get, szModuleName);
+		if (pModuleData)
+		{
+			if (O_dl_link_code_module(pModuleData) == GOOD)
 			{
-				pModule = getModule();
+				Init init = O_dl_find_function(0);
+				if (init)
+				{
+					pModule = init();
+					if (pModule)
+					{
+						ModuleEntry * pEntry = O_malloc(sizeof(ModuleEntry));
+						pEntry->pModule = pModule;
+						pEntry->pModuleData = pModuleData;
+						pEntry->szModuleName = O_strdup(szModuleName);
+						pEntry->uninit = O_dl_find_function(1);
+						pEntry->pNext = OOOF(pModules);
+						OOOF(pModules) = pEntry;
+					}
+				}
 			}
 		}
 	}
+
 	OOOICall(iRequirer, module, szModuleName, pModule);
 }
 OOOMethodEnd
